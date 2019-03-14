7696ab0fad8b064f31cb7890a549237f3c27c407
Andrew Patterson <andrewrpatterson@btinternet.com>
2019-03-14 13:17:31 +0000

    shouldve put it in this file

---
+++ fashion.py
@@ -0,0 +1,80 @@
+import argparse
+import time
+import keras
+import tensorflow as tf
+from time import time
+from keras.callbacks import TensorBoard
+
+
+
+# FashionMINST convolutional neural network, classify images of clothing into one of 10 classes.
+def network_one(learning_rate, epochs, batches):
+
+    fashion_mnist = keras.datasets.fashion_mnist
+
+    (train_images, train_labels), (test_images, test_labels) = fashion_mnist.load_data()
+
+    train_images = train_images / 255.0
+    test_images = test_images / 255.0
+
+
+    model = keras.Sequential([
+        keras.layers.Flatten(input_shape=(28, 28)),
+        keras.layers.Dense(128, activation=tf.nn.relu),
+        keras.layers.Dense(10, activation=tf.nn.softmax)
+    ])
+
+    model.compile(optimizer='SGD',
+                  loss='sparse_categorical_crossentropy',
+                  metrics=['accuracy'])
+
+    # Create a TensorBoard instance with the path to the logs directory
+    tensorboard = TensorBoard(log_dir='logs/{}'.format(time()))
+
+    model.fit(train_images, train_labels, epochs=5, callbacks=[tensorboard])
+
+    # Test the model on the test collection
+    test_loss, test_acc = model.evaluate(test_images, test_labels)
+
+    print('Test accuracy:', test_acc)
+
+def main(combination, learning_rate, epochs, batches, seed):
+
+    # Set Seed
+    print("Seed: {}".format(seed))
+
+    if int(combination)==1:
+        network_one(learning_rate, epochs, batches)
+    if int(combination)==2:
+        "fill me"
+
+    print("Done!")
+
+def check_param_is_numeric(param, value):
+
+    try:
+        value = float(value)
+    except:
+        print("{} must be numeric".format(param))
+        quit(1)
+    return value
+
+
+if __name__ == "__main__":
+
+    arg_parser = argparse.ArgumentParser(description="Assignment Program")
+    arg_parser.add_argument("combination", help="Flag to indicate which network to run")
+    arg_parser.add_argument("learning_rate", help="Learning Rate parameter")
+    arg_parser.add_argument("iterations", help="Number of iterations to perform")
+    arg_parser.add_argument("batches", help="Number of batches to use")
+    arg_parser.add_argument("seed", help="Seed to initialize the network")
+
+    args = arg_parser.parse_args()
+
+    combination = check_param_is_numeric("combination", args.combination)
+    learning_rate = check_param_is_numeric("learning_rate", args.learning_rate)
+    epochs = check_param_is_numeric("epochs", args.iterations)
+    batches = check_param_is_numeric("batches", args.batches)
+    seed = check_param_is_numeric("seed", args.seed)
+
+    main(combination, learning_rate, epochs, batches, seed)
