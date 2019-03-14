221526cf6d781b5fff2a9d9d0731f03f67d9e95e
Andrew Patterson <andrewrpatterson@btinternet.com>
2019-03-14 13:14:41 +0000

    feed forward implementation with keras/Tensorboard

--- assignment.py
+++ assignment.py
@@ -1,11 +1,42 @@
-import os
 import argparse
+import time
+import keras
+import tensorflow as tf
+from time import time
+from keras.callbacks import TensorBoard
 
 
+
+# FashionMINST convolutional neural network, classify images of clothing into one of 10 classes.
 def network_one(learning_rate, epochs, batches):
 
-    print("Combination One with learning rate: {} epochs: {} and batch size: {}".format(learning_rate, epochs, batches))
+    fashion_mnist = keras.datasets.fashion_mnist
 
+    (train_images, train_labels), (test_images, test_labels) = fashion_mnist.load_data()
+
+    train_images = train_images / 255.0
+    test_images = test_images / 255.0
+
+
+    model = keras.Sequential([
+        keras.layers.Flatten(input_shape=(28, 28)),
+        keras.layers.Dense(128, activation=tf.nn.relu),
+        keras.layers.Dense(10, activation=tf.nn.softmax)
+    ])
+
+    model.compile(optimizer='SGD',
+                  loss='sparse_categorical_crossentropy',
+                  metrics=['accuracy'])
+
+    # Create a TensorBoard instance with the path to the logs directory
+    tensorboard = TensorBoard(log_dir='logs/{}'.format(time()))
+
+    model.fit(train_images, train_labels, epochs=5, callbacks=[tensorboard])
+
+    # Test the model on the test collection
+    test_loss, test_acc = model.evaluate(test_images, test_labels)
+
+    print('Test accuracy:', test_acc)
 
 def network_two(learning_rate, epochs, batches):
 
