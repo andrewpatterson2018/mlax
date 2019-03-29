417e926040aa12177da4811426de002c1c994e83
Andrew Patterson <andrewrpatterson@btinternet.com>
2019-03-29 22:22:04 +0000

    changed this bash script to only contain the right run combos

--- run-all-combos.sh
+++ run-all-combos.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
-python fashion.py 4 0.1 20 500 12345
-python fashion.py 4 0.01 20 500 12345
+python fashion.py 1 0.1 20 500 12345
+python fashion.py 2 0.1 20 500 12345
 python fashion.py 3 0.1 20 500 12345
-python fashion.py 3 0.01 20 500 12345
