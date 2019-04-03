fd9ad0cb6ba101abc7b8957257c4e7b35df52d6f
Andrew Patterson <andrewrpatterson@btinternet.com>
2019-04-03 13:45:31 +0100

    changed the file so it puts to file and console, that way ye can check on the progress of the file just by looking at the console.

--- run-all-combos.py
+++ run-all-combos.py
@@ -1,18 +1,36 @@
-import fashion
-import imdb
+import sys, fashion, imdb
 
-# FASHION MNIST RUNS
-fashion.main(4,0.05,15,128,12345)
-fashion.main(4,0.01,15,256,12345)
-fashion.main(4,0.1,15,512,12345)
-fashion.main(2,0.05,15,128,12345)
-fashion.main(2,0.01,15,256,12345)
-fashion.main(2,0.1,15,512,12345)
 
-# IMDB RUNS
-imdb.main(3, 0.001, 15, 64, 12345)
-imdb.main(3, 0.1, 15, 128, 12345)
-imdb.main(3, 0.2, 15, 256, 12345)
-imdb.main(2, 0.001, 15, 64, 12345)
-imdb.main(2, 0.1, 15, 128, 12345)
-imdb.main(2, 0.2, 15, 256, 12345)
+class Logger(object):
+    def __init__(self):
+        self.terminal = sys.stdout
+        self.log = open("console-output.log", "a")
+
+    def write(self, message):
+        self.terminal.write(message)
+        self.log.write(message)  
+
+    def flush(self):
+        #this flush method is needed for python 3 compatibility.
+        #this handles the flush command by doing nothing.
+        #you might want to specify some extra behavior here.
+        pass    
+
+
+if __name__ == "__main__":
+    sys.stdout = Logger()
+    # FASHION MNIST RUNS
+    fashion.main(4,0.05,15,128,12345)
+    fashion.main(4,0.01,15,256,12345)
+    fashion.main(4,0.1,15,512,12345)
+    fashion.main(2,0.05,15,128,12345)
+    fashion.main(2,0.01,15,256,12345)
+    fashion.main(2,0.1,15,512,12345)
+
+    # IMDB RUNS
+    imdb.main(3, 0.001, 15, 64, 12345)
+    imdb.main(3, 0.1, 15, 128, 12345)
+    imdb.main(3, 0.2, 15, 256, 12345)
+    imdb.main(2, 0.001, 15, 64, 12345)
+    imdb.main(2, 0.1, 15, 128, 12345)
+    imdb.main(2, 0.2, 15, 256, 12345)
