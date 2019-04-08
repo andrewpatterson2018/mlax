7a607ddde2c55616b923944f293c9d78c41d8cb7
Andrew Patterson <andrewrpatterson@btinternet.com>
2019-04-08 17:50:42 +0100

    removed the output bug

--- imdb.py
+++ imdb.py
@@ -14,25 +14,16 @@ from keras.preprocessing import sequence
 
 def GetData():
     (trainX, trainY), (testX, testY) = imdb.load_data(path='imdb.pkl', num_words = 5000, seed=12345)
-    return trainX, testX, trainY, testY
+    return trainX, trainY, testX, testY
 
-def GetReportOutput(combination, learning_rate, batches, epochs, train_acc, test_acc):
-    final_combination_data = ("IMDB: Combination %d\nLearning Rate: %f, Number of Batches: %d, Number of Epochs: %d \nTraining Accuracy : %f, Test Accuracy : %f" % (combination, learning_rate, batches, epochs, train_acc, test_acc))
-    return(final_combination_data)
-
-def check_param_is_numeric(param, value):
-
-    try:
-        value = float(value)
-    except:
-        print("{} must be numeric".format(param))
-        quit(1)
-    return value
+def evaluate(model, train_x, train_y, test_x, test_y):
+    test_loss, test_acc = model.evaluate(test_x, test_y)
+    train_loss, train_acc = model.evaluate(train_x, train_y)
+    print("Train Loss: {}, Train Accuracy: {}; Test Loss: {}, Test Accuracy: {}".format(train_loss, train_acc, test_loss, test_acc))
 
 
 def cone(combination, _learning_rate, _epochs, _batches, _seed):
-    trainX, testX, trainY, testY = GetData()
-    numpy.random.seed(int(_seed))
+    trainX, trainY, testX, testY = GetData()
     max_review_length = 500
     top_words = 5000
     trainX = sequence.pad_sequences(trainX, maxlen=max_review_length)
@@ -44,26 +35,21 @@ def cone(combination, _learning_rate, _epochs, _batches, _seed):
     model.add(LSTM(100, dropout=0.2, recurrent_dropout=0.2))
     model.add(Dense(1, activation='sigmoid'))
     model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
-    print(model.summary())
 
-    # tensorboard
-    boardString = 'logs/imdb-{:d}-{:.3f}-{:d}-{:d}-{:d}.cpkt'.format(int(combination), _learning_rate, int(_epochs), int(_batches), int(_seed))
-    tensorboard = TensorBoard(log_dir=boardString, histogram_freq=2, write_grads=True)
+    tensorboard = buildTensorBoard(combination, _learning_rate, epochs, batches, seed)
+    
+    model.summary()
     
     model.fit(trainX, trainY, epochs=int(_epochs), batch_size=int(_batches), callbacks=[tensorboard], validation_data=(testX,testY))
+    
     # Final evaluation of the model
-    test_loss, test_acc = model.evaluate(testX, testY, verbose=0)
-    print('Test accuracy:', test_acc)
+    evaluate(model, trainX, trainY, testX, testY)
 
-    print(GetReportOutput(combination, _learning_rate, _batches, _epochs, train_acc, test_acc), 
-    file=open('imdb-{:d}-{:.3f}-{:d}-{:d}-{:d}-results.txt'.format(int(combination), _learning_rate, int(epochs), int(batches), int(seed)), 'w'))
-
-    model.save('imdb-{:d}-{:.3f}-{:d}-{:d}-{:d}.cpkt'.format(int(combination), _learning_rate, int(_epochs), int(_batches), int(_seed)))
+    model.save('imdb-{0}-{1}-{2}-{3}-{4}.cpkt'.format(combination, _learning_rate, epochs, batches, seed))
 
    
 def ctwo(combination, _learning_rate, _epochs, _batches, _seed):
-    trainX, testX, trainY, testY = GetData()
-    numpy.random.seed(int(_seed))
+    trainX, trainY, testX, testY = GetData()
     max_review_length = 500
     top_words = 5000
     embedding_vecor_length = 64
@@ -78,26 +64,29 @@ def ctwo(combination, _learning_rate, _epochs, _batches, _seed):
     model.add(LSTM(100))
     model.add(Dense(1, activation='sigmoid'))
     model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
-    print(model.summary())
+    
+    tensorboard = buildTensorBoard(combination, _learning_rate, epochs, batches, seed)
+    
+    model.summary()
+    
+    model.fit(trainX, trainY, epochs=int(_epochs), batch_size=int(_batches), callbacks=[tensorboard], validation_data=(testX,testY))
+    
+    # Final evaluation of the model
+    evaluate(model, trainX, trainY, testX, testY)
 
-    # tensorboard
-    boardString = 'logs/imdb-{:d}-{:.3f}-{:d}-{:d}-{:d}.cpkt'.format(int(combination), _learning_rate, int(_epochs), int(_batches), int(_seed))
+    model.save('imdb-{0}-{1}-{2}-{3}-{4}.cpkt'.format(combination, _learning_rate, epochs, batches, seed))
+
+
+
+def buildTensorBoard(combination, _learning_rate, epochs, batches, seed):
+    boardString = 'logs/imdb-{0}-{1}-{2}-{3}-{4}.cpkt'.format(combination, _learning_rate, epochs, batches, seed)
     tensorboard = TensorBoard(log_dir=boardString, histogram_freq=2, write_grads=True)
 
-    model.fit(trainX, trainY, epochs=int(_epochs), batch_size=int(_batches), callbacks=[tensorboard], validation_data=(testX,testY))
-    # Final evaluation of the model
-    test_loss, test_acc = model.evaluate(testX, testY, verbose=0)
-    print('Test accuracy:', test_acc)
-
-    print(GetReportOutput(combination, _learning_rate, _batches, _epochs, train_acc, test_acc), 
-    file=open('imdb-{:d}-{:.3f}-{:d}-{:d}-{:d}-results.txt'.format(int(combination), _learning_rate, int(epochs), int(batches), int(seed)), 'w'))
-
-    model.save('imdb-{:d}-{:.3f}-{:d}-{:d}-{:d}.cpkt'.format(int(combination), _learning_rate, int(_epochs), int(_batches), int(_seed)))
-
-
+    return tensorboard
 def main(combination, learning_rate, epochs, batches, seed):
-    # Set Seed
-    tf.random.set_random_seed(seed)
+   # Set Seed
+    numpy.random.seed(int(seed))
+    tf.random.set_random_seed(int(seed))
 
     print("Seed: {}".format(seed))
 
@@ -107,7 +96,14 @@ def main(combination, learning_rate, epochs, batches, seed):
         ctwo(combination, learning_rate, epochs, batches, seed)
 
 
+def check_param_is_numeric(param, value):
 
+    try:
+        value = float(value)
+    except:
+        print("{} must be numeric".format(param))
+        quit(1)
+    return value
 if __name__ == "__main__":
 
     arg_parser = argparse.ArgumentParser(description="Assignment Program")
