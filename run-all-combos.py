1174dad38bf4a43ade946e388bc2db01bd71e250
liam <liam_devlin@live.co.uk>
2019-04-09 14:06:13 +0100

    Updated all combos to run only new IMDB params

--- run-all-combos.py
+++ run-all-combos.py
@@ -19,24 +19,35 @@ class Logger(object):
 
 if __name__ == "__main__":
     sys.stdout = Logger()
-    # FASHION MNIST RUNS
-    # this combo gets 93% test accuracy after 13epochs
-    fashion.main(1,1,13,64,12345)
-    # how much did batch size affect performance of the network
-    fashion.main(1,1,13,256,12345)
-    # smaller learning rate
-    fashion.main(1,0.5,13,64,12345)
-    # smaller learning rate + bigger batch size
-    fashion.main(1,0.5,13,256,12345)
+    # # FASHION MNIST RUNS
+    # # this combo gets 93% test accuracy after 13epochs
+    # fashion.main(1,1,13,64,12345)
+    # # how much did batch size affect performance of the network
+    # fashion.main(1,1,13,256,12345)
+    # # smaller learning rate
+    # fashion.main(1,0.5,13,64,12345)
+    # # smaller learning rate + bigger batch size
+    # fashion.main(1,0.5,13,256,12345)
     
-    fashion.main(2,0.05,15,128,12345)
-    fashion.main(2,0.01,15,256,12345)
-    fashion.main(2,0.1,15,512,12345)
+    # fashion.main(2,0.05,15,128,12345)
+    # fashion.main(2,0.01,15,256,12345)
+    # fashion.main(2,0.1,15,512,12345)
 
     # IMDB RUNS
-    imdb.main(2, 0.3, 3, 256, 12345)
-    imdb.main(2, 0.05, 5, 512, 12345)
-    imdb.main(2, 0.1, 3, 64, 12345)
+    # imdb.main(2, 0.3, 3, 256, 12345)
+    # imdb.main(2, 0.05, 5, 512, 12345)
+    # imdb.main(2, 0.1, 3, 64, 12345)
+    # imdb.main(1, 0.3, 3, 256, 12345)
+    # imdb.main(1, 0.05, 5, 512, 12345)
+    # imdb.main(1, 0.1, 3, 64, 12345)
+
     imdb.main(1, 0.3, 3, 256, 12345)
     imdb.main(1, 0.05, 5, 512, 12345)
     imdb.main(1, 0.1, 3, 64, 12345)
+
+    imdb.main(2, 0.3, 3, 256, 12345)
+    imdb.main(2, 0.05, 5, 512, 12345)
+    imdb.main(2, 0.1, 3, 64, 12345)
+
+
+
